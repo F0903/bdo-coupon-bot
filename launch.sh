b45fd715008e1866459ad46f29819e3132fde4e3
Frederik P <frepbh@gmail.com>
2023-06-27 12:20:00 +0200

    updated launch file

--- launch.sh
+++ launch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 . .venv/bin/activate # Assumes there's a virtual environment called .venv
-pip install --upgrade -r requirements.txt
+pip install --upgrade --force-reinstall --no-cache-dir -r requirements.txt
 python -m bdo_coupon_bot
