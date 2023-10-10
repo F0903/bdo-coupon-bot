ae8494814b6014ced29dc0862b9e844ee4aa2a40
Frederik P <frepbh@gmail.com>
2023-10-10 22:48:19 +0200

    updated launch script

--- launch.sh
+++ launch.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-poetry run python -m bdo_coupon_bot
+poetry install
+
+if [ -f "python" ]
+then
+        poetry run python -m bdo_coupon_bot
+else
+        poetry run python3 -m bdo_coupon_bot
+fi
