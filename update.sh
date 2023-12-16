3fc6d6a991c1ce69f2d94fbf8e67771c8f78cf95
Frederik P. <frepbh@gmail.com>
2023-12-16 17:34:43 +0100

    updated update script

--- update.sh
+++ update.sh
@@ -4,10 +4,10 @@ if [[ $(git diff --name-only) ]]; then
     echo "Installing new changes."
     git reset --hard origin/master
     poetry install
-fi
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    echo "Performing linux specific changes."
-    chmod +x update.sh
-    chmod +x launch.sh
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        echo "Performing linux specific changes."
+        chmod +x update.sh
+        chmod +x launch.sh
+    fi
 fi
