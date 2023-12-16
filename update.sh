864b465b2a4066a04ba04331b19e42ff68e12b9e
Frederik P. <frepbh@gmail.com>
2023-12-16 17:23:33 +0100

    updates

--- update.sh
+++ update.sh
@@ -7,6 +7,7 @@ if [[ $(git diff --name-only) ]]; then
 fi
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    echo "Performing linux specific changes."
     chmod +x update.sh
     chmod +x launch.sh
 fi
